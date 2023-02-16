826dd5597b94e65e3d1f54b08f683b7589b3eea0
wuqi <1379121780@qq.com>
2023-02-16 09:14:22 +0800

    删除没必要的文件文件夹；增加布局视图组件；添加依赖

---
+++ src/views/layout/layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="educationLayout">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'education-layout'
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import './layout.scss'
+</style>
